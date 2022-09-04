dd9d7ebd357d21f12b1d0ab08b1b83e065c9a53a
snezhka003 <snezhanka19@mail.ru>
2022-09-04 20:38:24 +0300

    Задача № 3 готова

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,25 @@
+getHole = index => document.getElementById(`hole${index}`);
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+
+for (let i = 1; i < 10; i++) {
+    getHole(i).onclick = function() {
+        if (getHole(i).className.includes('hole_has-mole')) {
+            dead.textContent = Number(dead.textContent) + 1;
+        } else {
+            lost.textContent = Number(lost.textContent) + 1;
+        }
+
+        if (dead.textContent === '10') {
+            setTimeout(() => {
+                alert("Вы выиграли! Поздравляем!!");
+                dead.textContent = 0;
+            });
+        } else if (lost.textContent === '5') {
+            setTimeout(() => {
+                alert("Вы проиграли! Можете попробовать снова.");
+                lost.textContent = 0;
+            });
+        }
+    }
+};
