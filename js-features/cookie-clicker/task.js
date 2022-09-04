5fe5e10420c2ee1743dd4647dd48b085f9062268
snezhka003 <snezhanka19@mail.ru>
2022-09-04 19:11:13 +0300

    Задачи № 1 и № 2 готовы

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,7 +1,19 @@
 const clickerCounter = document.getElementById("clicker__counter");
 const image = document.getElementById("cookie");
+const speed = document.getElementById("clicker__speed");
+
+let timeNow = Date.now();
 
 image.onclick = function() {
-    clickerCounter.textContent += 1;
-    image.weight = 150;
+    clickerCounter.textContent = Number(clickerCounter.textContent) + 1;
+
+    if(image.getAttribute('width') === '200') { 
+        image.width = Number(image.width) + 100;
+    } else {
+        image.width = Number(image.width) - 100;
+    };
+    
+    let lastClick = Date.now();
+    speed.textContent = (1 / ((lastClick - timeNow) / 1000)).toFixed(2);
+    timeNow = Date.now();
 };
--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -3,22 +3,30 @@ const timerCount = function() {
     timer.textContent -= 1;
 
     if (timer.textContent === '0') {
-        window.alert("Вы победили в конкурсе!");
+        setTimeout(() => window.alert("Вы победили в конкурсе!"));
         clearInterval(intervalId);
     }
 }
 
 let intervalId = setInterval(timerCount, 1000);
 
-// Задача № 2 со звездочкой
-// const timerCount = function() {
-//     const timer = document.getElementById("timer");
-//     timer.textContent -= 1;
+// Задачи № 1 и № 2 со звездочкой
+// const timer = document.getElementById("timer");
+// let seconds = Number(timer.textContent);
+// timer.textContent = getTime();
 
-//     if (timer.textContent === '0') {
-//         window.location = "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1dkiqarAOlVu6vOivfn8-6WVPt5AV8h6y";
-//         clearTimeout(timeoutId);
+// function getTime() {
+//     return new Date(seconds * 1000).toISOString().substring(11, 19);
+// } 
+
+// const timerCount = function() {
+//     seconds -= 1;
+//     timer.textContent = getTime();
+
+//     if (timer.textContent === '00:00:00') {
+//         setTimeout(() => window.location = "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1dkiqarAOlVu6vOivfn8-6WVPt5AV8h6y");
+//         clearInterval(intervalId);
 //     }
 // }
 
-// let timeoutId = setInterval(timerCount, 1000);
+// let intervalId = setInterval(timerCount, 1000);
