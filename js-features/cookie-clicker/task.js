915d0e3b8154da5ce12375c2060ac84550ebdc91
snezhka003 <snezhanka19@mail.ru>
2022-09-06 20:45:52 +0300

    Задача № 2 - правки

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -5,13 +5,7 @@ const speed = document.getElementById("clicker__speed");
 let timeNow = Date.now();
 
 image.onclick = function() {
-    clickerCounter.textContent = Number(clickerCounter.textContent) + 1;
-
-    if(image.getAttribute('width') === '200') { 
-        image.width = Number(image.width) + 100;
-    } else {
-        image.width = Number(image.width) - 100;
-    };
+    image.width = ++clickerCounter.textContent % 2 ? 250 : 200;
     
     let lastClick = Date.now();
     speed.textContent = (1 / ((lastClick - timeNow) / 1000)).toFixed(2);
