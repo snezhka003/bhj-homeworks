eafd8affe5b3c92aa3b2bb82362a6b9df4fc4ddf
snezhka003 <snezhanka19@mail.ru>
2022-09-07 22:19:05 +0300

    Задача № 1 - правки

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,17 +1,18 @@
 const popup = document.getElementById('modal_main');
-popup.className = "modal modal_active";
-let popupClose = document.getElementsByClassName('modal__close');
-let arr = Array.from(popupClose);
+let popupClose = Array.from(document.getElementsByClassName('modal__close'));
 let showSuccess = document.getElementsByClassName('show-success');
 let popupSuccess = document.getElementById('modal_success');
 
-for (let i = 0; i < arr.length; i++) {
-    arr[i].onclick = function() {
+popup.className = "modal modal_active";
+
+popupClose.forEach((item) => {
+    item.onclick = function() {
         popup.className = "modal";
         popupSuccess.className = "modal";
     }
-}
+})
 
 showSuccess[0].onclick = function() {
+    popup.className = "modal";
     popupSuccess.className = "modal modal_active";
 }
