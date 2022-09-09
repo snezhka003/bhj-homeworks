ac840af05afbe1e5ffa5e0aa98228cd0a9410ec0
snezhka003 <snezhanka19@mail.ru>
2022-09-09 19:08:36 +0300

    Задача № 2 - правки

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,17 +2,18 @@ const menuLink = Array.from(document.querySelectorAll(".menu__link"));
 const menuList = Array.from(document.querySelectorAll(".menu"));
 
 menuLink.forEach((item) => {
-    item.onclick = function() {        
-        menuList.forEach((element) => {
-            if (element.classList.contains("menu_sub")) {
-                element.classList.toggle("menu_active");
+    item.onclick = function() {
+        let parentMenuLink = item.parentElement.querySelector(".menu");
 
-                return false;
-            };
+        if (parentMenuLink) {
+            if (parentMenuLink.classList.contains("menu_active")) {
+                parentMenuLink.classList.remove("menu_active");                
+            } else {
+                menuList.forEach((element) => element.classList.remove("menu_active"));
+                parentMenuLink.classList.add("menu_active");                
+            }
 
-            if (element.classList.contains("menu_active")) {
-                element.classList.remove("menu_active");
-            };
-        });
+            return false;
+        };
     };
 })
