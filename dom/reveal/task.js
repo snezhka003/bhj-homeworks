f482c657ff319d4524c3ea4cbfec1ec9fe83e0b4
snezhka003 <snezhanka19@mail.ru>
2022-09-17 12:48:24 +0300

    Задача № 1  - готова

---
+++ dom/reveal/task.js
@@ -0,0 +1,27 @@
+const reveal = Array.from(document.querySelectorAll('.reveal'));
+
+function isVisible(item) {
+    const { top, bottom } = item.getBoundingClientRect(item);
+
+    if (top > window.innerHeight) {
+        return false;
+    }
+
+    if (bottom < 0) {
+        return false;
+    }
+
+    return true;
+};
+
+window.addEventListener('scroll', () => {
+    reveal.forEach(element => {
+        element.classList.remove("reveal_active");
+
+        if (isVisible(element)) {
+            element.classList.add("reveal_active");
+        } else {
+            element.classList.remove("reveal_active");
+        }
+    });
+});
