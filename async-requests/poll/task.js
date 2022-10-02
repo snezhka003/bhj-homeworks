904833e105fbda79ca137797f6dd9dc0872ffec8
snezhka003 <snezhanka19@mail.ru>
2022-10-03 00:58:16 +0300

    Задача № 2 - правки

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -5,9 +5,9 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
 xhr.send();
-xhr.addEventListener('readystatechange', () => {
+xhr.addEventListener('load', () => {
     if ((xhr.readyState === xhr.DONE && xhr.status === 200)) {
-        let data = JSON.parse(xhr.response);
+        const data = JSON.parse(xhr.response);
         
         pollTitle.textContent = data.data.title;
 
@@ -24,17 +24,17 @@ xhr.addEventListener('readystatechange', () => {
             btn.addEventListener('click', (event) => {
                 alert("Спасибо, ваш голос засчитан!");
 
-                let request = new XMLHttpRequest();
-                let index = data.data['answers'].indexOf(event.target);
-                let sendForm = `vote=${data.id}&answer=${index}`;
+                const request = new XMLHttpRequest();
+                const index = data.data['answers'].indexOf(event.target);
+                const sendForm = `vote=${data.id}&answer=${index}`;
                 
                 request.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
                 request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
                 request.send(sendForm);
 
-                request.addEventListener('readystatechange', () => {
+                request.addEventListener('load', () => {
                     if ((request.readyState === request.DONE && request.status === 200)) {
-                        let responseData = JSON.parse(request.response);
+                        const responseData = JSON.parse(request.response);
 
                         function getSum() {
                             let count = 0;
@@ -49,7 +49,7 @@ xhr.addEventListener('readystatechange', () => {
                         pollAnswers.remove();
 
                         responseData.stat.forEach(element => {
-                            let votesPersent = ((element.votes / getSum()) * 100).toFixed(2);
+                            const votesPersent = ((element.votes / getSum()) * 100).toFixed(2);
 
                             pollTitle.insertAdjacentHTML('afterEnd', `
                             <p class="answer__title">
