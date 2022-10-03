09830675033452e9e271fdc2f8a920a642cfc604
snezhka003 <snezhanka19@mail.ru>
2022-10-03 14:07:04 +0300

    Задача № 1  - правки

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,16 +1,13 @@
 const editor = document.getElementById('editor');
-const editorText = window.localStorage.getItem('text');
 const btnClear = document.getElementById('clear');
 
-if (editorText) {
-    editor.value = editorText;
-} 
+editor.value = window.localStorage.getItem('text');
 
-editor.addEventListener("change", () => {
+editor.addEventListener("input", () => {
     window.localStorage.setItem('text', editor.value);
 });
 
 btnClear.addEventListener('click', () => {
     window.localStorage.removeItem('text');
     editor.value = '';
-})
+});
