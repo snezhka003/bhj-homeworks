2de2929135f40f64e1182030f98694183725fe2c
snezhka003 <snezhanka19@mail.ru>
2022-10-04 00:47:57 +0300

    Задача № 2 - правки

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -2,63 +2,88 @@ const pollTitle = document.getElementById('poll__title');
 const pollAnswers = document.getElementById('poll__answers');
 
 const xhr = new XMLHttpRequest();
+xhr.responseType = 'json';
+
+// const createRequest = async (options = {url, headers, method, body,}) => {
+//     let xhr = new XMLHttpRequest();
+//     xhr.responseType = 'json';
+
+//     xhr.open(method, url);
+//     xhr.setRequestHeader(headers);
+//     xhr.send(body); 
+// };
+
+// createRequest({
+//     url: 'https://netology-slow-rest.herokuapp.com/poll.php',
+//     method: 'GET',
+// });
 
 xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
 xhr.send();
 xhr.addEventListener('load', () => {
-    if ((xhr.readyState === xhr.DONE && xhr.status === 200)) {
-        const data = JSON.parse(xhr.response);
+    const data = xhr.response;
         
-        pollTitle.textContent = data.data.title;
+    pollTitle.textContent = data.data.title;
 
-        data.data['answers'].forEach(item => {
-            pollAnswers.insertAdjacentHTML('afterBegin', `
-            <button class="poll__answer">
-                ${item}
-            </button>`)
-        });
+    data.data['answers'].forEach(item => {
+        //вариант 1
+        let btnPollAnswer = document.createElement('button');
+        btnPollAnswer.className = "poll__answer";
+        btnPollAnswer.innerHTML = `${item}`;
+        pollAnswers.insertAdjacentElement('afterBegin', btnPollAnswer);
 
-        const btnAnswer = Array.from(document.querySelectorAll('.poll__answer'));
+        // вариант 2
+        // pollAnswers.insertAdjacentHTML('afterBegin', `
+        // <button class="poll__answer">
+        //     ${item}
+        // </button>`)
+    });
 
-        btnAnswer.forEach(btn => {
-            btn.addEventListener('click', (event) => {
-                alert("Спасибо, ваш голос засчитан!");
+    const btnAnswer = Array.from(document.querySelectorAll('.poll__answer'));
 
-                const request = new XMLHttpRequest();
-                const index = data.data['answers'].indexOf(event.target);
-                const sendForm = `vote=${data.id}&answer=${index}`;
+    btnAnswer.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            alert("Спасибо, ваш голос засчитан!");
+
+            const request = new XMLHttpRequest();
+            request.responseType = 'json';
+            const index = data.data['answers'].indexOf(event.target);
+            const sendForm = `vote=${data.id}&answer=${index}`;
+            
+            request.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
+            request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            request.send(sendForm);
+
+            request.addEventListener('load', () => {
+                const responseData = request.response;
+
+                function getSum() {
+                    let count = 0;
+                    
+                    responseData.stat.forEach(element => {
+                        count += element.votes;   
+                    });
+
+                    return count;
+                };
                 
-                request.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
-                request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                request.send(sendForm);
+                pollAnswers.remove();
 
-                request.addEventListener('load', () => {
-                    if ((request.readyState === request.DONE && request.status === 200)) {
-                        const responseData = JSON.parse(request.response);
+                responseData.stat.forEach(element => {
+                    const votesPersent = ((element.votes / getSum()) * 100).toFixed(2);
+                    //вариант 1
+                    let answerTittle = document.createElement('p');
+                    answerTittle.className = "answer__title";
+                    answerTittle.innerHTML = `${element.answer}: <span style="font-weight:bold">${votesPersent}%</span>`;
+                    pollTitle.insertAdjacentElement('afterEnd', answerTittle);
 
-                        function getSum() {
-                            let count = 0;
-                            
-                            responseData.stat.forEach(element => {
-                                count += element.votes;   
-                            });
-
-                            return count;
-                        };
-                        
-                        pollAnswers.remove();
-
-                        responseData.stat.forEach(element => {
-                            const votesPersent = ((element.votes / getSum()) * 100).toFixed(2);
-
-                            pollTitle.insertAdjacentHTML('afterEnd', `
-                            <p class="answer__title">
-                                ${element.answer}: <span style="font-weight:bold">${votesPersent}%</span>
-                            </p>`)  
-                        });
-                    };
+                    // вариант 2
+                    // pollTitle.insertAdjacentHTML('afterEnd', `
+                    // <p class="answer__title">
+                    //     ${element.answer}: <span style="font-weight:bold">${votesPersent}%</span>
+                    // </p>`)  
                 });
             });
         });
-    };
+    });
 });
