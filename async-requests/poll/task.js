dec3c39cb2591cde25b2c60c2360a019d0949d6a
snezhka003 <snezhanka19@mail.ru>
2022-10-05 17:51:08 +0300

    Задача № 2 - правки, вроде окончательные

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -6,18 +6,13 @@ function createRequest(options = {}) {
     xhr.responseType = 'json';
 
     xhr.open(options.method, options.url);
-    xhr.setRequestHeader(options.headers);
+    xhr.setRequestHeader('Content-type', options.header);
     xhr.send(options.body);
     xhr.addEventListener('load', () => {
         options.callback(xhr.response);
     });
-}
+};
 
-// const xhr = new XMLHttpRequest();
-// xhr.responseType = 'json';
-
-// xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
-// xhr.send();
 createRequest({
     url: 'https://netology-slow-rest.herokuapp.com/poll.php',
     method: 'GET',
@@ -30,17 +25,10 @@ function createBtn(data) {
     pollTitle.textContent = data.data.title;
 
     data.data['answers'].forEach(item => {
-        //вариант 1
         let btnPollAnswer = document.createElement('button');
         btnPollAnswer.className = "poll__answer";
         btnPollAnswer.innerHTML = `${item}`;
         pollAnswers.insertAdjacentElement('afterBegin', btnPollAnswer);
-
-        // вариант 2
-        // pollAnswers.insertAdjacentHTML('afterBegin', `
-        // <button class="poll__answer">
-        //     ${item}
-        // </button>`)
     });
 
     const btnAnswer = Array.from(document.querySelectorAll('.poll__answer'));
@@ -54,7 +42,7 @@ function createBtn(data) {
 
             createRequest({
                 url: 'https://netology-slow-rest.herokuapp.com/poll.php',
-                headers: `'Content-type', 'application/x-www-form-urlencoded'`,
+                header: 'application/x-www-form-urlencoded',
                 method: 'POST',
                 body: sendForm,
                 callback: (response) => {
@@ -62,13 +50,6 @@ function createBtn(data) {
                     showVotes(response); 
                 }
             });
-
-            // const request = new XMLHttpRequest();
-            // request.responseType = 'json';
-            
-            // request.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
-            // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-            // request.send(sendForm);
 
             function showVotes(responseData) {
                 function getSum() {
@@ -85,17 +66,10 @@ function createBtn(data) {
 
                 responseData.stat.forEach(element => {
                     const votesPersent = ((element.votes / getSum()) * 100).toFixed(2);
-                    //вариант 1
                     let answerTittle = document.createElement('p');
                     answerTittle.className = "answer__title";
                     answerTittle.innerHTML = `${element.answer}: <span style="font-weight:bold">${votesPersent}%</span>`;
-                    pollTitle.insertAdjacentElement('afterEnd', answerTittle);
-
-                    // вариант 2
-                    // pollTitle.insertAdjacentHTML('afterEnd', `
-                    // <p class="answer__title">
-                    //     ${element.answer}: <span style="font-weight:bold">${votesPersent}%</span>
-                    // </p>`)  
+                    pollTitle.insertAdjacentElement('afterEnd', answerTittle); 
                 });
             };
         });
